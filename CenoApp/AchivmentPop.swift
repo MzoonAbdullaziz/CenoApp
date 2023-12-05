0d3267e626a29e540859f7cd43f6e11adaef9fc1
Sarajay19 <sarajayg19@gmail.com>
2023-12-05 17:29:03 +0300

    I added the achivment pop message page

---
+++ CenoApp/AchivmentPop.swift
@@ -0,0 +1,18 @@
+//
+//  AchivmentPop.swift
+//  CenoApp
+//
+//  Created by Sara AlQuwaifli on 05/12/2023.
+//
+
+import SwiftUI
+
+struct AchivmentPop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchivmentPop()
+}
