416adf6bb94076d5b5a4fde8b126afca842caba2
Sarajay19 <sarajayg19@gmail.com>
2023-12-05 17:21:08 +0300

    I add the Avatar View

---
+++ CenoApp/Avatar.swift
@@ -0,0 +1,18 @@
+//
+//  Avatar.swift
+//  CenoApp
+//
+//  Created by Sara AlQuwaifli on 05/12/2023.
+//
+
+import SwiftUI
+
+struct Avatar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Avatar()
+}
