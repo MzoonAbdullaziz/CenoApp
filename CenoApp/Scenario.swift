e738563f9498a7a4a0c309c2378a48e64dc7d4fc
Sarajay19 <sarajayg19@gmail.com>
2023-12-05 21:56:40 +0300

    made new view for scenario page

---
+++ CenoApp/Scenario.swift
@@ -0,0 +1,18 @@
+//
+//  Scenario.swift
+//  CenoApp
+//
+//  Created by Sara AlQuwaifli on 05/12/2023.
+//
+
+import SwiftUI
+
+struct Scenario: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scenario()
+}
