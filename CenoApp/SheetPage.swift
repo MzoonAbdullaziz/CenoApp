cc3506df170482c4432276e74d75dfe81022751b
Mayaner <mem.kpope@gmail.com>
2023-12-05 17:58:16 +0300

    sheetpage

---
+++ CenoApp/SheetPage.swift
@@ -0,0 +1,18 @@
+//
+//  SheetPage.swift
+//  CenoApp
+//
+//  Created by Mayasem Muner on 12/5/23.
+//
+
+import SwiftUI
+
+struct SheetPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetPage()
+}
