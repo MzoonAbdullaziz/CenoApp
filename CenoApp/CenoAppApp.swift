aa22e28497e0f199db9cf41d00ac59521662f4ea
MzoonAbdullaziz <mzoonabdullaziz8@gmail.com>
2023-12-05 17:03:29 +0300

    Initial Commit

---
+++ CenoApp/CenoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenoAppApp.swift
+//  CenoApp
+//
+//  Created by Mzoon AlRubaish on 21/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CenoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CenoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CenoApp
+//
+//  Created by Mzoon AlRubaish on 21/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
